e5859f178d1160cc6c45fa10289c61bee7f94a2b
kawaldhillon05 <kawaldhillon5@gmail.com>
2023-10-16 23:10:30 -0400

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	if(a == ""){
+    return 0;
+}  else {
+    return (a.reduce(myFunction));
+    function myFunction(total, value) {
+    return total + value;
+    }
+}
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let n  = 1;
+    for(i=0;i<a.length;i++){
+        console.log(n = n*a[i]);
+    }
+    return n;
+}
 
+const power = function(a,b) {
+	return a**b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+	  if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorial(num - 1));
+    }
+  }
 
 // Do not edit below this line
 module.exports = {
