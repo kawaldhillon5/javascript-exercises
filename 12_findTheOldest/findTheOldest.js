566385172f2035ece6768006bae82f3b03eacd7b
kawaldhillon05 <kawaldhillon5@gmail.com>
2023-10-27 18:29:19 -0400

    completed all exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peoples) {
+    if(!peoples.yearOfDeath){
+        peoples.yearOfDeath = 2023;
+    }
 
+    peoples.sort((a,b) =>{
+        if(!a.yearOfDeath){
+            a.yearOfDeath = 2023;
+        };
+        if(!b.yearOfDeath){
+            b.yearOfDeath = 2023;
+        };
+        const a_lived = a.yearOfDeath - a.yearOfBirth;
+        const b_lived = b.yearOfDeath - b.yearOfBirth;
+        return a_lived - b_lived;
+    });
+    return peoples[peoples.length-1];
 };
 
 // Do not edit below this line
